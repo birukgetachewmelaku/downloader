f7f48d2dbc9c3d5db660d51811966d50e31529a7
birukgetachewmelaku <birukgetachewmelaku@gmail.com>
2024-01-06 19:30:10 +0300

    adding get mthods

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 
 const app = express();
 
-app.get('/download', (req, res) => {
-  const filePath = 'xampp.exe'; // Replace with the actual file path
+app.get('/download_mysql', (req, res) => {
+  const filePath = 'mysql.zip'; // Replace with the actual file path
 
   const file = path.resolve(filePath);
   const filename = path.basename(file);
@@ -14,7 +14,79 @@ app.get('/download', (req, res) => {
   const ranges = rangeParser(fs.statSync(file).size, req.headers.range || '');
 
   if (!ranges) {
-    console.log("LLLLLLL")
+    res.status(416).send('Range Not Satisfiable'); 
+    return;
+  }
+
+  res.setHeader('Accept-Ranges', 'bytes');
+  res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+
+  if (ranges.length === 1) {
+    const { start, end } = ranges[0];
+    const stream = fs.createReadStream(file, { start, end });
+    stream.on('error', (err) => {
+      res.status(500).send('Error reading the file');
+    });
+    res.status(206);
+    res.setHeader('Content-Range', `bytes ${start}-${end}/${fs.statSync(file).size}`);
+    res.setHeader('Content-Length', end - start + 1);
+    stream.pipe(res);
+  } else {
+    const stream = fs.createReadStream(file);
+    stream.on('error', (err) => {
+      res.status(500).send('Error reading the file');
+    });
+    res.status(200);
+    res.setHeader('Content-Length', fs.statSync(file).size);
+    stream.pipe(res);
+  }
+});
+
+app.get('/download_php', (req, res) => {
+  const filePath = 'php.zip'; // Replace with the actual file path
+
+  const file = path.resolve(filePath);
+  const filename = path.basename(file);
+
+  const ranges = rangeParser(fs.statSync(file).size, req.headers.range || '');
+
+  if (!ranges) {
+    res.status(416).send('Range Not Satisfiable'); 
+    return;
+  }
+
+  res.setHeader('Accept-Ranges', 'bytes');
+  res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+
+  if (ranges.length === 1) {
+    const { start, end } = ranges[0];
+    const stream = fs.createReadStream(file, { start, end });
+    stream.on('error', (err) => {
+      res.status(500).send('Error reading the file');
+    });
+    res.status(206);
+    res.setHeader('Content-Range', `bytes ${start}-${end}/${fs.statSync(file).size}`);
+    res.setHeader('Content-Length', end - start + 1);
+    stream.pipe(res);
+  } else {
+    const stream = fs.createReadStream(file);
+    stream.on('error', (err) => {
+      res.status(500).send('Error reading the file');
+    });
+    res.status(200);
+    res.setHeader('Content-Length', fs.statSync(file).size);
+    stream.pipe(res);
+  }
+});
+app.get('/download_Apache24', (req, res) => {
+  const filePath = 'Apache24.zip'; // Replace with the actual file path
+
+  const file = path.resolve(filePath);
+  const filename = path.basename(file);
+
+  const ranges = rangeParser(fs.statSync(file).size, req.headers.range || '');
+
+  if (!ranges) {
     res.status(416).send('Range Not Satisfiable'); 
     return;
   }
